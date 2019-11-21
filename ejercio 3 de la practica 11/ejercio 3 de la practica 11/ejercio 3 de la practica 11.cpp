34d88dc4bdaa024f200d4f9b88d05df5a5789327
Alumno <Alumno@PC24>
2019-11-21 12:18:01 -0600

    s

---
+++ ejercio 3 de la practica 11/ejercio 3 de la practica 11/ejercio 3 de la practica 11.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void numeracion()
+{
+	int num,suma, b;	float v[100] = {};
+	for (b = 0; b <= 100; b++)
+	{
+		cout << b << endl;
+	}
+}
+int main()
+{
+	numeracion();
+}
